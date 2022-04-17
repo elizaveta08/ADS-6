180c373c158f4d89c36242fc28894b8f213e0bb0
Sakhno Elizaveta <97175260+elizaveta08@users.noreply.github.com>
2022-04-17 04:32:25 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,33 +1,35 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {// реализация шаблона очереди с приоритетом на кольцевом буфере
    private:
-     T arr[100];
-     int first;  // Указатель на первый элемент очереди
-     int last;   // Указатель на конец очереди
+      T arr[100];
+      int first;  // Указатель на первый элемент очереди
+      int last;   // Указатель на конец очереди
+     
    public:
-      TQueue():first(0), last(0) { }
-     void push(T x) {
-         if (last - first >= size)
-             throw std::string("Full!");
-         else
-             arr[(last++) % size] = x;
-     }
-     T pop() {
-       return arr[(first++) % size];
-     }
-     int getSize() {
-       return (last-first);
-     }
-     T front() {
-         return arr[first%size];
-     }
-     T back() {
-         return arr[(last-1)%size];
-     }
+       TPQueue():first(0), last(0) { }
+      void push(T x) {
+          if (last - first >= size)
+              throw std::string("Full!");
+          else
+              arr[(last++) % size] = x;
+      }
+      T pop() {
+        return arr[(first++) % size];
+      }
+      int getSize() {
+        return (last-first);
+      }
+      T front() {
+          return arr[first%size];
+      }
+      T back() {
+          return arr[(last-1)%size];
+      }
 }
 struct SYM {
   char ch;
